cdf2c47843aea3027b522f57132b45e3653cfb63
Galuh Ramadhan <galuh.html@gmail.com>
2022-02-14 13:37:34 +0700

    write some tests

---
+++ src/components/CardList.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import CardList from './CardList';
+
+it('expect to render CardList component', () => {
+    const mockRobots = [
+        {
+            id: 1,
+            name: 'John Snow',
+            username: 'johns',
+            email: 'john@gmail.com'
+        }
+    ];
+    expect(shallow(<CardList robots={mockRobots} />)).toMatchSnapshot();
+});
